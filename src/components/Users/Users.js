d59cbf74210f6b944de7d86bf49be0d02bdbc437
AlexGithubbb <31776215+AlexGithubbb@users.noreply.github.com>
2019-08-22 16:31:14 -0400

    modify responsive

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,19 +1,20 @@
-import React, {useContext} from 'react';
+import React, { useContext } from 'react';
 import UserItem from './UserItem';
 import Spinner from '../layout/Spinner';
 // import PropTypes from 'prop-types';
+import './Users.css';
 import GithubContext from '../../context/github/githubContext';
 
-
 const Users = () => {
-  const githubContext = useContext(GithubContext)
-  const { loading, users} = githubContext;
+  const githubContext = useContext(GithubContext);
+  const { loading, users } = githubContext;
 
   if (loading) {
     return <Spinner />;
   } else {
     return (
-      <div style={userStyle}>
+      // <div style={userStyle}>
+      <div className='userStyle'>
         {users.map(user => (
           <UserItem key={user.id} user={user} />
         ))}
